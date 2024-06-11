e87beafb6a6af23d3ecb10654e070ef84bc00356
DZ-staking <112197568+FCPIDZ213@users.noreply.github.com>
2024-06-12 00:07:15 +0200

    Update NavFooter.vue

--- src/layouts/components/NavFooter.vue
+++ src/layouts/components/NavFooter.vue
@@ -1,35 +1,15 @@
 <template>
   <!-- footer -->
-  <footer
-    class="flex items-center h-12 mt-5 text-sm bg-gray-100 dark:bg-[#171d30] py-2 z-10 w-full"
-  >
+  <footer class="flex items-center h-12 mt-5 text-sm bg-gray-100 dark:bg-[#171d30] py-2 z-10 w-full">
     <div class="flex flex-1">
-      &copy;&nbsp;
-      {{ new Date().getFullYear() }}&nbsp;
-      Made With&nbsp;
-      <img src="../../assets/images/heart.svg" />&nbsp;
-      By&nbsp;
-      <a
-        class="link link-primary no-underline"
-        href="https://ping.pub"
-        target="_blank"
-        rel="noopener noreferrer"
-        >Ping.pub</a
-      >
+      &copy;&nbsp;{{ new Date().getFullYear() }}&nbsp;Made With&nbsp;<img src="../../assets/images/heart.svg" />&nbsp;By&nbsp;
+      <a class="link link-primary no-underline" href="https://ping.pub" target="_blank" rel="noopener noreferrer">Ping.pub</a>
     </div>
-    <div
-      class="hidden md:!block"
-    >
-      <a
-        class="link link-primary no-underline"
-        href="https://github.com/FCPIDZ213"
-        target="noopener noreferrer"
-        >DZ-staking Github</a
-      >
+    <div class="hidden md:block">
+      <a class="link link-primary no-underline" href="https://github.com/FCPIDZ213" target="_blank" rel="noopener noreferrer">DZ-staking Github</a>
     </div>
   </footer>
 </template>
-
 
 
 
